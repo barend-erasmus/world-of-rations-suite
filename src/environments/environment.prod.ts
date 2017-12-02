bdb844e52a20dde97c10032f52afa50c09a41b3c
developersworkspace@gmail.com <developersworkspace@gmail.com>
2017-12-02 21:29:39 +0200

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   application: {
     id: 1,
-    uri: 'https://suite.worldofrations',
+    uri: 'https://suite.worldofrations.com',
   },
   api: {
     uri: 'https://api.suite.worldofrations.com/api',

9a61e33990b204c96067d02b6fc03af158673e47
Barend Erasmus <developersworkspace@gmail.com>
2017-12-06 08:14:05 +0200

    implements formulator

--- src/app/formulator-route/formulator-route.component.ts
+++ src/app/formulator-route/formulator-route.component.ts
@@ -13,10 +13,17 @@ export class FormulatorRouteComponent implements OnInit {
 
   public user: any = {};
 
+  public ingredients: any[] = [];
+
   public dietGroupDropdowns: any[] = [];
 
   public selectedDiet: any = null;
+
   public diets: any[] = [];
+
+  public formulationIngredients: any[] = [];
+
+  public mixWeight = 1000;
 
   constructor(private http: Http) { }
 
@@ -24,6 +31,8 @@ export class FormulatorRouteComponent implements OnInit {
     this.user = JSON.parse(localStorage.getItem('user'));
 
     this.loadDietGroupDropdown(null);
+
+    this.loadIngredients();
   }
 
   public onChange_DietGroupDropdown(index: number): void {
@@ -34,6 +43,24 @@ export class FormulatorRouteComponent implements OnInit {
     this.loadDietGroupDropdown(this.dietGroupDropdowns[index].selectedItem.id);
   }
 
+  public onClick_Formulate(): void {
+    console.log(this.selectedDiet);
+    const headers = new Headers();
+    headers.append('x-application-id', environment.application.id.toString());
+    headers.append('authorization', `Bearer ${localStorage.getItem('token')}`);
+
+    this.http.post(`${environment.api.uri}/formulator/create`, {
+      diet: this.selectedDiet,
+      formulationIngredients: this.formulationIngredients,
+      mixWeight: this.mixWeight,
+    }, {
+        headers,
+      })
+      .map((res: Response) => res.json()).subscribe((json) => {
+        console.log(json);
+      });
+  }
+
   private onDietGroupSelected(): void {
 
     const headers = new Headers();
@@ -41,11 +68,12 @@ export class FormulatorRouteComponent implements OnInit {
     headers.append('authorization', `Bearer ${localStorage.getItem('token')}`);
 
     this.http.get(`${environment.api.uri}/diet/list` +
-    `?dietGroupId=${this.dietGroupDropdowns[this.dietGroupDropdowns.length - 1].selectedItem.id}`, {
-      headers,
-    })
+      `?dietGroupId=${this.dietGroupDropdowns[this.dietGroupDropdowns.length - 1].selectedItem.id}`, {
+        headers,
+      })
       .map((res: Response) => res.json()).subscribe((json) => {
         this.diets = json;
+        this.selectedDiet = this.diets[0];
       });
   }
 
@@ -66,6 +94,127 @@ export class FormulatorRouteComponent implements OnInit {
         } else {
           this.onDietGroupSelected();
         }
+      });
+  }
+
+  private loadIngredients(): void {
+    const headers = new Headers();
+    headers.append('x-application-id', environment.application.id.toString());
+    headers.append('authorization', `Bearer ${localStorage.getItem('token')}`);
+
+    this.http.get(`${environment.api.uri}/ingredient/list`, {
+      headers,
+    })
+      .map((res: Response) => res.json()).subscribe((json) => {
+        this.ingredients = json;
+
+        this.formulationIngredients = [
+          {
+            ingredient: this.ingredients.find((x) => x.name === 'Alfalfa hay, dehy 17% CP'),
+            cost: 2300,
+            minimum: 0,
+            maximum: this.mixWeight,
+            weight: null,
+          },
+          {
+            ingredient: this.ingredients.find((x) => x.name === 'Sunflower seed hulls'),
+            cost: 800,
+            minimum: 0,
+            maximum: this.mixWeight,
+            weight: null,
+          },
+          {
+            ingredient: this.ingredients.find((x) => x.name === 'Corn grain (maize), yellow'),
+            cost: 3800,
+            minimum: 0,
+            maximum: this.mixWeight,
+            weight: null,
+          },
+          {
+            ingredient: this.ingredients.find((x) => x.name === 'Oats, grain'),
+            cost: 3000,
+            minimum: 0,
+            maximum: this.mixWeight,
+            weight: null,
+          },
+          {
+            ingredient: this.ingredients.find((x) => x.name === 'Molasses, sugarcane (syrup)'),
+            cost: 4500,
+            minimum: 0,
+            maximum: this.mixWeight * 0.08,
+            weight: null,
+          },
+          {
+            ingredient: this.ingredients.find((x) => x.name === 'Fish meal, herring'),
+            cost: 6000,
+            minimum: 0,
+            maximum: this.mixWeight,
+            weight: null,
+          },
+          {
+            ingredient: this.ingredients.find((x) => x.name === 'Cottonseed o/c meal, mech extr'),
+            cost: 7000,
+            minimum: 0,
+            maximum: this.mixWeight * 0.120,
+            weight: null,
+          },
+          {
+            ingredient: this.ingredients.find((x) => x.name === 'Urea 46% Nitrogen'),
+            cost: 6000,
+            minimum: 0,
+            maximum: this.mixWeight * 0.004,
+            weight: null,
+          },
+          {
+            ingredient: this.ingredients.find((x) => x.name === 'Canola (Rapeseed) oil'),
+            cost: 7500,
+            minimum: 0,
+            maximum: this.mixWeight,
+            weight: null,
+          },
+          {
+            ingredient: this.ingredients.find((x) => x.name === 'Ca, as ground Limestone'),
+            cost: 1200,
+            minimum: 0,
+            maximum: this.mixWeight,
+            weight: null,
+          },
+          {
+            ingredient: this.ingredients.find((x) => x.name === 'Na and Cl, as Salt'),
+            cost: 900,
+            minimum: this.mixWeight * 0.004,
+            maximum: this.mixWeight * 0.004,
+            weight: null,
+          },
+          {
+            ingredient: this.ingredients.find((x) => x.name === 'K, as Potassium Chloride (KCl)'),
+            cost: 2000,
+            minimum: 0,
+            maximum: this.mixWeight,
+            weight: null,
+          },
+          {
+            ingredient: this.ingredients.find((x) => x.name === 'Mg, as Magnesium oxide (MgO)'),
+            cost: 2500,
+            minimum: 0,
+            maximum: this.mixWeight,
+            weight: null,
+          },
+          {
+            ingredient: this.ingredients.find((x) => x.name === 'Ca and P, as Di-calcium phosphate'),
+            cost: 2500,
+            minimum: 0,
+            maximum: this.mixWeight,
+            weight: null,
+          },
+          {
+            ingredient: this.ingredients.find((x) => x.name === 'S, as Ammonium Sulphate ((NH4)2SO4)'),
+            cost: 5000,
+            minimum: 0,
+            maximum: this.mixWeight,
+            weight: null,
+          },
+        ];
       });
   }
 
